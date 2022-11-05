f140d29e6ac516b03d566622c54c59e8ae5c314f
Kasey Doan <kaseydoan@ucr-secure-31-10-13-174-188.wnet.ucr.edu>
2022-11-05 13:24:04 -0700

    add grids

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import{ Button, Typography, Box } from '@mui/material'
+import{ Button, Typography, Grid} from '@mui/material'
 import { Link } from "react-router-dom";
 import { Symptom } from './components/Symptom';
 import { green } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
 
 function App() {
 
@@ -21,20 +22,36 @@ function App() {
   <Button
     variant="contained"
     component={Link} to={system.name}
+    sx = {{
+        bgcolor: 'primary.main',
+        boxShadow: 8,
+        borderRadius: 19,
+        p: 2,
+        minWidth: 300,
+
+    }}
   >
     {system.name}
   </Button>
   ) 
   );
-
   return (
+
     <div className="App">
-      <Box sx={{ width: '100%', maxWidth: 10000 }}>
-      <Typography variant="h4" className = "heading" >MedMe</Typography>
+      
+      <Grid container spacing={5} justifyContent="center" >
+        <Grid item xs={12}>
+          <Typography variant="h1" className = "heading" >MedMe</Typography>
+        </Grid>
+        <Grid item xs={3.5}>
+          <img src = "https://img.favpng.com/7/2/8/human-anatomy-human-body-nervous-system-illustration-png-favpng-JWdvkggrxPxrQyvbGvH1ivQjC.jpg" alt="Main Page Photo" width = "300" height = "300"/>
+        </Grid>
+        <Grid item xs={3.5}>
+          {bodySystemsList}
+        </Grid>
+      </Grid>
+        
 
-        {bodySystemsList}
-
-      </Box>
     </div>
   );
 }
