417e92a57e892dbbfbf4df5f9c32db42a58ca990
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2022-11-05 10:39:47 -0700

    Routing with Buttons

--- src/components/Symptom.js
+++ src/components/Symptom.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Symptom = () => {
   return (
     <div>
-        <h1>Symptom</h1>
+        <h1>Symptoms</h1>
     </div>
   )
 }
