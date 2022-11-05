f3d9a33af8da64c16692a94e63d75c271b7566b0
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2022-11-05 10:30:18 -0700

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Link } from "react-router-dom";
+import { Symptom } from './components/Symptom';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <li>
+        <Link to="/Symptom">Symptoms</Link>
+      </li>
     </div>
   );
 }
