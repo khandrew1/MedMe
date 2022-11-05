abf3921042efae057df6a04172c2fa03d905a824
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2022-11-05 11:37:41 -0700

    refactored Button code

--- src/App.js
+++ src/App.js
@@ -2,32 +2,40 @@ import './App.css';
 import{ Button, Typography, Box } from '@mui/material'
 import { Link } from "react-router-dom";
 import { Symptom } from './components/Symptom';
-import { Button } from '@mui/material'
 
 function App() {
+
+  const bodySystemsData = [
+    { name: "Skeletal" },
+    { name: "Muscular" },
+    { name: "Nervous" },
+    { name: "Cardiovascular" },
+    { name: "Respiratory" },
+    { name: "Digestive" },
+    { name: "Urinary" },
+    { name: "Reproductive" },
+  ]
+
+  const bodySystemsList = bodySystemsData.map((system) => (
+  <Button
+    variant="contained"
+    component={Link} to={system.name}
+  >
+    {system.name}
+  </Button>
+  ) 
+  );
+
   return (
     <div className="App">
       <Box sx={{ width: '100%', maxWidth: 10000 }}>
         <Typography variant="h4" gutterBottom>
           MedMe
         </Typography>
-        <Button variant="contained" href="">Skeletal</Button><br></br>
-        <Button variant="contained" href="">Muscular</Button><br></br>
-        <Button variant="contained" href="">Nervous</Button><br></br>
-        <Button variant="contained" href="">Cardiovascular</Button><br></br>
-        <Button variant="contained" href="">Respiratory</Button><br></br>
-        <Button variant="contained" href="">Digestive</Button><br></br>
-        <Button variant="contained" href="">Urinary</Button><br></br>
-        <Button variant="contained" href="">Reproductive</Button><br></br>
 
-       
-
-
+        {bodySystemsList}
 
       </Box>
-
-
-
     </div>
   );
 }
---
+++ src/components/systems/Digestive.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Digestive = () => {
+  return (
+    <div>Digestive</div>
+  )
+}
