b0695f4b494b302f6b311eda516b43dd1d11f19d
Kasey Doan <kaseydoan@ucr-secure-31-10-13-174-188.wnet.ucr.edu>
2022-11-05 16:44:28 -0700

    running out of time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import{ Button, Typography, Grid} from '@mui/material'
+import{ Button, Typography, Grid,} from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import { Link } from "react-router-dom";
-import { Symptom } from './components/Symptom';
-import { green } from '@mui/material/colors';
-import { createTheme } from '@mui/material/styles';
 import mainPagePhoto from './images/mainPagePhoto.png';
+
 
 function App(props) {
 
@@ -13,7 +14,8 @@ function App(props) {
     variant="contained"
     component={Link} to={system.name}
     sx = {{
-        bgcolor: 'primary.main',
+        textAlign: 'center',
+        bgcolor: '#2E3B55',
         boxShadow: 8,
         borderRadius: 19,
         p: 1,
@@ -28,12 +30,22 @@ function App(props) {
   ) 
   );
   return (
-
     <div className="App">
-      
       <Grid container spacing={5} justifyContent="center" >
         <Grid item xs={12}>
-          <Typography variant="h1" className = "heading" >MedMe</Typography>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style = {{background: '#2E3B55'}}>
+        <Toolbar>
+          <Typography variant="header" component="div" className = "heading" 
+          sx={{ 
+            flexGrow: 2,
+            }}>
+            MedMe
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+    </Box>
         </Grid>
         <Grid item xs={3.5}>
           <img src = {mainPagePhoto} alt="Main Page Photo" className = "widthSet"/>
@@ -42,9 +54,9 @@ function App(props) {
           {bodySystemsList}
         </Grid>
       </Grid>
-        
-
     </div>
+
+   
   );
 }
 
