4dbdf38a9378bb3950fa675ac2182ed7ac248c6c
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2022-11-05 10:46:33 -0700

    Link Tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Link } from "react-router-dom";
 import { Symptom } from './components/Symptom';
+import { Button } from '@mui/material'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Button
+        component={Link} to="/Symptom"
+        > Bro </Button>
       <li>
         <Link to="/Symptom">Symptoms</Link>
       </li>
